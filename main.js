9b66cc8d1545f833591d3ac88713dd65e2fc8e47
Administrator <2076212263@qq.com>
2023-11-09 12:52:10 +0800

    更新脚本

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-
-
-
 const cookieName = '小懒工具箱'
 const tokenKey = 'tl_sign'
 
-const chavy = init()
-const tokenVal = $request.headers['token']
-if (tokenVal) {
 
-  if (chavy.setdata(tokenVal, tokenKey)) {
-    chavy.msg(`${cookieName}`, '获取Token: 成功', `token: ${tokenVal}`)
-    chavy.log(`[${cookieName}] 获取Token: 成功, token: ${tokenVal}`)
-  }
+const chavy = init()
+const tokenVal = chavy.getdata(tokenKey)
+
+sign()
+
+function sign() {
+  let url = { url: `https://wxapp.xiaolankj.top/api/app/user.php?act=userCheckIn`, headers: { } }
+  url.headers['token'] = `${tokenVal}`
+  url.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.43(0x18002b29) NetType/WIFI Language/zh_CN'
+  chavy.get(url, (error, response, data) => {
+    const result = JSON.parse(data)
+    const title = `${cookieName}`
+    let subTitle = ``
+    let detail = ``
+    if (result.code === 2) {
+
+        subTitle = `签到结果: 成功`
+        detail = `说明: ${result.msg}`
+        chavy.msg(title, subTitle, detail)
+    }
+    chavy.log(`${cookieName}, data: ${data}`)
+  })
 }
+
 
 function init() {
   isSurge = () => {
@@ -57,4 +70,3 @@ function init() {
   }
   return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
 }
-chavy.done()
